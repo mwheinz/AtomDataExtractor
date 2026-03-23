1475d5d77bfee2c8d681701945116a5230e23e63
Michael Heinz <michael.william.heinz@gmail.com>
2026-03-23 14:57:02 -0400

    Fix a bug when building in Linux.

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,4 @@ rm -rf build dist ade.spec
 
 echo "_version = '$(git describe --tags --always)'" >adeversion.py
 pyinstaller --optimize 2 --windowed ade.py --icon=../resources/MyIcon.icns
-pyinstaller --optimize 2 --windowed adv.py --icon=../resources/MyIcon.icns
+pyinstaller --optimize 2 --windowed adv.py --icon=../resources/MyIcon.icns --hidden-import='PIL._tkinter_finder'
